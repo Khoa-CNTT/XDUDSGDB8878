2695ff247687163b85f91e5d68bea6b7144b741d
Văn Mạnh <manhsubcheo2@gmail.com>
2025-03-26 14:14:21 +0700

    API Contract UploadFile Word And Change Status Contract

--- advanced-real-estate-app/src/screens/user/auth/SignIn.jsx
+++ advanced-real-estate-app/src/screens/user/auth/SignIn.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {appInfo} from "../../../constants/appInfos";
-
+import Toast from "../../../config/ToastConfig";
+import handleAPI from '../../../apis/handlAPI';
+import { useDispatch } from "react-redux";
+import { addAuth } from "../../../redux/reducers/authReducer"; // Import addAuth action
+import { useNavigate } from 'react-router-dom';
 const SignIn = () => {
+    const [customer, setCustomer] = useState({});
+    const navigate = useNavigate();
+    const dispatch = useDispatch(); // Sử dụng useDispatch để tạo dispatch
+
+    const hanlderLogin = async () => {
+        const url = `/api/customers/login`;
+        try {
+            const res = await handleAPI(url,customer , "post");
+            console.log(res.token);
+            
+            if(res.status === 200) {
+                Toast("success", res.message);
+                // Lưu thông tin xác thực vào localStorage
+                const authData = {
+                    token: res?.token,
+                    roles: "CUSTOMER",
+                    info: "",
+                    permission : []
+                };
+                // Dispatch action để lưu vào Redux store
+                dispatch(addAuth(authData));
+                // Điều hướng về trang /admin ngay lập tức
+                navigate("/");
+            }
+        } catch (error) {
+            Toast("error", error.message);
+        }
+    }
 
     return (
         <div style={
@@ -38,7 +70,7 @@ const SignIn = () => {
                                     animationName: "fadeInUp"
                                 }}
                             >
-                                <form>
+                                <div>
                                     <div className="row g-3">
                                         <div className="col-md-12">
                                             <div className="form-floating">
@@ -47,6 +79,11 @@ const SignIn = () => {
                                                     className="form-control"
                                                     id="email"
                                                     placeholder="Your Email"
+                                                    value={customer.email || ''}
+                                                    onChange={(e) => setCustomer({
+                                                        ...customer,
+                                                        email: e.target.value
+                                                    })}
                                                 />
                                                 <label htmlFor="email">Email</label>
                                             </div>
@@ -58,18 +95,23 @@ const SignIn = () => {
                                                     className="form-control"
                                                     id="password"
                                                     placeholder="Your password"
+                                                    value={customer.password || ''}
+                                                    onChange={(e) => setCustomer({
+                                                        ...customer,
+                                                        password: e.target.value
+                                                    })}
                                                 />
                                                 <label htmlFor="password">Mật khẩu</label>
                                             </div>
                                         </div>
 
                                         <div className="col-12">
-                                            <button className="btn btn-primary w-100 py-3" type="submit">
+                                            <button className="btn btn-primary w-100 py-3" onClick={() => hanlderLogin()}>
                                                 ĐĂNG NHẬP
                                             </button>
                                         </div>
                                     </div>
-                                </form>
+                                </div>
                             </div>
                         </div>
                     </div>
